3776abf9ce19056213e9c8706a050a3ab5674397
hoangtulaptrinh <duong98kk@gmail.com>
2020-06-18 20:15:53 +0700

    handle basic socket io

--- Back-End/index.js
+++ Back-End/index.js
@@ -1,22 +1,27 @@
-const express = require('express')
-var bodyParser = require('body-parser')
-const app = express()
-app.use(bodyParser.json())
-const _ = require('lodash')
-var objData = require('./data/objData')
-var recipes = require('./data/recipes')
-var elements = require('./data/newElements')
-var originObjData = require('./data/originObjData')
+const express = require("express");
+const socket = require("socket.io");
+const http = require("http");
+var bodyParser = require("body-parser");
+const app = express();
+app.use(bodyParser.json());
+const _ = require("lodash");
+var objData = require("./data/objData");
+var recipes = require("./data/recipes");
+var elements = require("./data/newElements");
+var originObjData = require("./data/originObjData");
 
-app.get('/', (req, res) => {
-  res.send('hello from server!')
-})
+const server = http.Server(app);
+const io = socket(server);
+
+app.get("/", (req, res) => {
+  res.send("hello from server!");
+});
 //gửi danh sách các nguyên tố ban đầu sang bên front-end
-app.get('/api/getdata', (req, res) => {
-  res.json(objData)
-})
+app.get("/api/getdata", (req, res) => {
+  res.json(objData);
+});
 
-app.get('/api/get_new_item', (req, res) => {
+app.get("/api/get_new_item", (req, res) => {
   var idDrop = Number(req.query.idDrop);
   const nameDrag = req.query.nameItemDrag;
   const nameDrop = req.query.nameItemDrop;
@@ -24,40 +29,51 @@ app.get('/api/get_new_item', (req, res) => {
   var objDrop = _.find(recipes, { [nameDrop]: {} }); ////obj có key là nguyên tố bị thả vào
   var arrElements;
   if (_.has(objDrag, [nameDrag, nameDrop]) === true) {
-    arrElements = _.find(elements, (val) => val.name == objDrag[nameDrag][nameDrop]);
+    arrElements = _.find(
+      elements,
+      (val) => val.name == objDrag[nameDrag][nameDrop]
+    );
   }
   if (_.has(objDrag, [nameDrag, nameDrop]) === false) {
-    arrElements = _.find(elements, (val) => val.name == objDrop[nameDrop][nameDrag]);
+    arrElements = _.find(
+      elements,
+      (val) => val.name == objDrop[nameDrop][nameDrag]
+    );
   }
   if (arrElements !== undefined) {
     var newItemsObj = {
       id: objData.Items.length,
       name: arrElements.name,
       url: arrElements.url,
-    }
+    };
     if (idDrop < 4) {
       objData.Items = _.concat(objData.Items, newItemsObj);
-      objData.Target = _.concat(objData.Target,
-        {
-          id: objData.Target.length,
-          name: arrElements.name,
-          url: arrElements.url,
-          recipe: `${objData.Items[req.query.idDrag].name} + ${objData.Target[idDrop].name}`
-        })
-    }
-    else {
+      objData.Target = _.concat(objData.Target, {
+        id: objData.Target.length,
+        name: arrElements.name,
+        url: arrElements.url,
+        recipe: `${objData.Items[req.query.idDrag].name} + ${
+          objData.Target[idDrop].name
+        }`,
+      });
+    } else {
       objData.Items = _.concat(objData.Items, newItemsObj);
-      _.fill(objData.Target,
+      _.fill(
+        objData.Target,
         {
           id: idDrop,
           name: arrElements.name,
           url: arrElements.url,
-          recipe: `${objData.Items[req.query.idDrag].name} + ${objData.Target[idDrop].name}`
-        }
-        , idDrop, idDrop + 1)
+          recipe: `${objData.Items[req.query.idDrag].name} + ${
+            objData.Target[idDrop].name
+          }`,
+        },
+        idDrop,
+        idDrop + 1
+      );
     }
     const ids = new Set();
-    const newItems = objData.Items.filter(e => {
+    const newItems = objData.Items.filter((e) => {
       if (ids.has(e.name)) {
         return false;
       } else {
@@ -66,19 +82,18 @@ app.get('/api/get_new_item', (req, res) => {
       }
     });
     objData.Items = newItems;
-    res.json(objData)
+    res.json(objData);
   }
-})
-app.post('/api/create_item', (req, res) => {
-  objData.Items = _.concat(objData.Items,
-    {
-      id: objData.Items.length,
-      name: req.body.name,
-      url: req.body.url,
-    });
+});
+app.post("/api/create_item", (req, res) => {
+  objData.Items = _.concat(objData.Items, {
+    id: objData.Items.length,
+    name: req.body.name,
+    url: req.body.url,
+  });
 
   const ids = new Set();
-  const newItems = objData.Items.filter(e => {
+  const newItems = objData.Items.filter((e) => {
     if (ids.has(e.name)) {
       return false;
     } else {
@@ -88,10 +103,10 @@ app.post('/api/create_item', (req, res) => {
   });
   objData.Items = newItems;
 
-  res.json(objData)
-})
+  res.json(objData);
+});
 
-app.delete('/api/change/delete', (req, res) => {
+app.delete("/api/change/delete", (req, res) => {
   //trừ id của từng item đi 1 kể từ item bị xóa để code hoạt động bình thường
   _.forEach(objData.Items, (item, index) => {
     if (index > req.query.id) {
@@ -103,34 +118,50 @@ app.delete('/api/change/delete', (req, res) => {
     return item.name === req.query.name;
   });
 
-  res.json(objData)
+  res.json(objData);
+});
 
-})
-
-app.patch('/api/change_item', (req, res) => {
-
-  _.fill(objData.Items,
+app.patch("/api/change_item", (req, res) => {
+  _.fill(
+    objData.Items,
     {
       id: req.body.id,
       name: req.body.name,
       url: req.body.url,
-    }
-    , req.body.id, req.body.id + 1)
+    },
+    req.body.id,
+    req.body.id + 1
+  );
 
-  res.json(objData)
-})
+  res.json(objData);
+});
 
-app.get('/api/change/reset', (req, res) => {
+app.get("/api/change/reset", (req, res) => {
   objData.Target = originObjData.Target;
-  res.json(objData)
-})
+  res.json(objData);
+});
 
-app.get('/api/change/hardreset', (req, res) => {
+app.get("/api/change/hardreset", (req, res) => {
   objData.Items = originObjData.Items;
   objData.Target = originObjData.Target;
-  res.json(objData)
-})
+  res.json(objData);
+});
 
-app.listen(5000, () => {
-  console.log('App listening on port 5000')
-})
+//socket.io
+io.on("connection", (socket) => {
+  console.log(`vừa có người kết nối và id là : ${socket.id}`);
+  socket.on("Client-send-data", (data) => {
+    //Case 2: socket.emit là sever chỉ gửi cho chính người gửi lên thôi VD: A gửi => sever gửi lại A
+    socket.emit("Sever-send-data", { target: "you", messenger: data });
+
+    //Case 3: socket.broadcast.emit là sever cho tất cả mọi người trừ người gửi VD: A gửi => sever gửi lại cho tất cả mọi người trừ A
+    socket.broadcast.emit("Sever-send-data", {
+      target: "some_body",
+      messenger: data,
+    });
+  });
+});
+//socket.io
+server.listen(5000, () => {
+  console.log("App listening on port 5000");
+});
--- Little-Alchemy/src/App.js
+++ Little-Alchemy/src/App.js
@@ -1,90 +1,186 @@
-import React, { Component } from 'react';
-import './App.css';
-import Target from './components/Target'
-import Item from './components/Item'
-import { Container, Row, Col, Button } from 'reactstrap';
-import * as actions from './actions/index';
-import { connect } from 'react-redux';
-import CreateNewItems from './components/createElements'
+import React, { Component } from "react";
+import { MessageCircle, Send } from "react-feather";
+import socketIOClient from "socket.io-client";
+import { connect } from "react-redux";
+import {
+  Container,
+  Row,
+  Col,
+  Button,
+  Popover,
+  PopoverHeader,
+  PopoverBody,
+  Input,
+} from "reactstrap";
 
-import * as callApi from './axios/axios'
+import * as actions from "./actions/index";
+import CreateNewItems from "./components/createElements";
+import * as callApi from "./axios/axios";
+import "./App.css";
+import Target from "./components/Target";
+import Item from "./components/Item";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      inputValue: "",
+      listMessenger: [],
+      popoverOpen: false,
+    };
+  }
   ChangeItems = (idDrag) => {
     var idDrop = this.props.IdDrop;
     var objChange = {
       nameItemDrag: this.props.Arr.Items[idDrag].name,
       nameItemDrop: this.props.Arr.Target[idDrop].name,
       idDrop: idDrop,
-      idDrag: idDrag
+      idDrag: idDrag,
     };
-    callApi.Params('/api/get_new_item', 'get', objChange).then(response => this.props.FETCH_DATA(response.data))
-  }
+    callApi
+      .Params("/api/get_new_item", "get", objChange)
+      .then((response) => this.props.FETCH_DATA(response.data));
+  };
 
   deleteItems = (idDrag, nameDrag) => {
     var objDelete = {
       id: idDrag,
-      name: nameDrag
-    }
+      name: nameDrag,
+    };
 
-    callApi.Params('/api/change/delete', 'delete', objDelete).then(response => this.props.FETCH_DATA(response.data))
-  }
-  updateIdDrop = idDrop => {
+    callApi
+      .Params("/api/change/delete", "delete", objDelete)
+      .then((response) => this.props.FETCH_DATA(response.data));
+  };
+  updateIdDrop = (idDrop) => {
     this.props.updateID(idDrop);
-  }
+  };
 
   reset = () => {
-    callApi.Params('/api/change/reset', 'get', null).then(response => this.props.FETCH_DATA(response.data))
-  }
+    callApi
+      .Params("/api/change/reset", "get", null)
+      .then((response) => this.props.FETCH_DATA(response.data));
+  };
 
   hardReset = () => {
-    callApi.Params('/api/change/hardreset', 'get', null).then(response => this.props.FETCH_DATA(response.data))
-  }
+    callApi
+      .Params("/api/change/hardreset", "get", null)
+      .then((response) => this.props.FETCH_DATA(response.data));
+  };
+
+  socket = socketIOClient("http://localhost:5000/");
 
   componentDidMount() {
-    callApi.Params('/api/getdata', 'get', null).then(response => this.props.FETCH_DATA(response.data))
+    this.socket.on("Sever-send-data", (data) => {
+      this.setState({
+        listMessenger: [...this.state.listMessenger, data],
+      });
+    });
+    callApi
+      .Params("/api/getdata", "get", null)
+      .then((response) => this.props.FETCH_DATA(response.data));
   }
 
   render() {
-    const { Arr } = this.props
+    const { inputValue, listMessenger, popoverOpen } = this.state;
+    const { Arr } = this.props;
+    const toggle = () =>
+      this.setState({
+        popoverOpen: !popoverOpen,
+      });
+    const sendMessenger = () =>
+      this.socket.emit("Client-send-data", inputValue);
     return (
       <div className="App">
-
-        <div className='total-target'>
-          <h2>Drop Below Me</h2>
-
-          <div className='drop-Target'>
-            <Container id='Container'>
-              <Row id='row'>
-
-                {
-                  Arr.Target.map((item, index) => (
-                    <Col sm="2" key={index}>
-                      <Target item={item} key={index} name={item.name} url={item.url} recipe={item.recipe} updateIdDrop={(id) => this.updateIdDrop(id)} />
-                    </Col>
-                  ))
-                }
-
+        <div className="total-target">
+          <div className="total-header">
+            <MessageCircle
+              className="icon-mess"
+              id="Popover1"
+              size="50"
+              color="blue"
+            />
+            <h2>Drop Below Me</h2>
+            <Popover
+              placement="bottom"
+              isOpen={popoverOpen}
+              target="Popover1"
+              toggle={toggle}
+            >
+              <PopoverHeader>Chat Box</PopoverHeader>
+              <PopoverBody>
+                <div className="content-chat">
+                  {!!listMessenger.length &&
+                    listMessenger.map((item, index) => (
+                      <div
+                        className={`messenger ${
+                          item.target === "you"
+                            ? "your-messenger"
+                            : "some-body-messenger"
+                        }`}
+                        key={index}
+                      >
+                        <span className="title">
+                          {item.target === "you" ? "You" : "Some Body"}
+                        </span>
+                        <span className="content">{item.messenger}</span>
+                      </div>
+                    ))}
+                </div>
+                <div className="input-chat">
+                  <Input
+                    value={inputValue}
+                    onChange={(e) =>
+                      this.setState({
+                        inputValue: e.target.value,
+                      })
+                    }
+                  />
+                  <Send size="35" color="blue" onClick={sendMessenger} />
+                </div>
+              </PopoverBody>
+            </Popover>
+          </div>
+          <div className="drop-Target">
+            <Container id="Container">
+              <Row id="row">
+                {Arr.Target.map((item, index) => (
+                  <Col sm="2" key={index}>
+                    <Target
+                      item={item}
+                      key={index}
+                      name={item.name}
+                      url={item.url}
+                      recipe={item.recipe}
+                      updateIdDrop={(id) => this.updateIdDrop(id)}
+                    />
+                  </Col>
+                ))}
               </Row>
             </Container>
-
           </div>
           <Button outline color="danger" onClick={() => this.reset()}>
             Clear Screen
-          </Button>{' '}
+          </Button>{" "}
           <Button outline color="danger" onClick={() => this.hardReset()}>
             Study again from the beginning
-          </Button>{' '}
+          </Button>{" "}
         </div>
 
         <div className="item-container">
           <CreateNewItems />
           {Arr.Items.map((item, index) => (
-            <Item item={item} key={index} name={item.name} url={item.url} ChangeItems={(id) => this.ChangeItems(id)} deleteItems={() => this.deleteItems(item.id, item.name)} />
+            <Item
+              item={item}
+              key={index}
+              name={item.name}
+              url={item.url}
+              ChangeItems={(id) => this.ChangeItems(id)}
+              deleteItems={() => this.deleteItems(item.id, item.name)}
+            />
           ))}
         </div>
-
       </div>
     );
   }
@@ -94,14 +190,22 @@ const mapStatetoProps = (state) => {
   return {
     IdDrop: state.IdDrop,
     Arr: state.Arr,
-  }
-}
+  };
+};
 const mapDispatchToProps = (dispatch) => {
   return {
     updateID: (idDrop) => dispatch(actions.updateID({ idDrop: idDrop })),
-    followRecipe: (name, url, idDrag, idDrop) => dispatch(actions.followRecipe({ name: name, url: url, idDrag: idDrag, idDrop: idDrop })),
-    FETCH_DATA: (data) => dispatch(actions.FETCH_DATA({ data: data }))
-  }
-}
+    followRecipe: (name, url, idDrag, idDrop) =>
+      dispatch(
+        actions.followRecipe({
+          name: name,
+          url: url,
+          idDrag: idDrag,
+          idDrop: idDrop,
+        })
+      ),
+    FETCH_DATA: (data) => dispatch(actions.FETCH_DATA({ data: data })),
+  };
+};
 
 export default connect(mapStatetoProps, mapDispatchToProps)(App);
